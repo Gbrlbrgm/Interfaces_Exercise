277bd5f712868ba01c8e7688608bf1da7baeda5a
Gbrlbrgm <gabriel_bergamo@outlook.com>
2021-07-04 00:02:42 -0300

    Project Started

---
+++ Fixacao/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Fixacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
